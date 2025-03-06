ef74bac955d58f53ce790634c44d89e307ab7ea2
Muhammad shahzad <shehzadmemon147@gmail.com>
2025-03-06 14:46:36 +0500

    learn ts

--- Lecture-1/app.ts
+++ Lecture-1/app.ts
@@ -1,14 +1,13 @@
-// function add(num1: number, num2: number, printResult: boolean, sometext: string) {
-//     if (printResult) {
-//         console.log(`${sometext} ${num1 + num2}`);
-//     }else{
-//         return num1 + num2;
-//     }
-// }
+function add(num1: number, num2: number, printResult: boolean, sometext: string) {
+    if (printResult) {
+        console.log(`${sometext} ${num1 + num2}`);
+    }else{
+        return num1 + num2;
+    }
+}
 
-// const printResult = true
-// const sometext = "Sum of two number"
-// const n1 = 5;
-// const n2 = 10;
-// add(n1, n2, printResult, sometext);
-// 
+const printResult = true
+const sometext = "Sum of two number"
+const n1 = 5;
+const n2 = 10;
+add(n1, n2, printResult, sometext);
--- Lecture-2/app.ts
+++ Lecture-2/app.ts
@@ -1,16 +1,16 @@
 // obj array tuple enum 
-// const person : {
-//     name: string;
-//     age: number;
-//     xyz:{
-//         address: string;
-//     }
-// } = {
-//     name: "John",
-//     age: 25,
-//     xyz:{
-//         address: "New York"
-//     }
-// }
-// console.log(person.xyz.address)
-// 
+
+const person : {
+    name: string;
+    age: number;
+    xyz:{
+        address: string;
+    }
+} = {
+    name: "John",
+    age: 25,
+    xyz:{
+        address: "New York"
+    }
+}
+console.log(person.xyz.address)
