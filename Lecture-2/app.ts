ccd8231f2b958a6659a3a953f00a47156b770561
Muhammad shahzad <shehzadmemon147@gmail.com>
2025-03-06 14:39:54 +0500

    learn ts

--- Lecture-2/app.ts
+++ Lecture-2/app.ts
@@ -1,15 +1,15 @@
 // obj array tuple enum 
-const person : {
-    name: string;
-    age: number;
-    xyz:{
-        address: string;
-    }
-} = {
-    name: "John",
-    age: 25,
-    xyz:{
-        address: "New York"
-    }
-}
-console.log(person.xyz.address)
+// const person : {
+//     name: string;
+//     age: number;
+//     xyz:{
+//         address: string;
+//     }
+// } = {
+//     name: "John",
+//     age: 25,
+//     xyz:{
+//         address: "New York"
+//     }
+// }
+// console.log(person.xyz.address)
