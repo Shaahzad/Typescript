9ad2604a831194b9f50008cd02517cb581a5b3c9
Muhammad shahzad <shehzadmemon147@gmail.com>
2025-03-06 17:12:13 +0500

    learn ts

--- Lecture-2/app.js
+++ Lecture-2/app.js
@@ -1,9 +1,58 @@
-// obj array tuple enum 
-var person = {
-    name: "John",
-    age: 25,
-    xyz: {
-        address: "New York"
-    }
-};
-console.log(person.xyz.address);
+// obj array tuple enum
+// obj
+// const person : {
+//     name: string;
+//     age: number;
+//     xyz: {
+//         address: string;
+//     }
+// } = {
+//     name: "John",
+//     age: 25,
+//     xyz:{
+//         address: "karachi"
+//     },
+// }
+// console.log(person) 
+// array
+// const person : {
+//     name: string;
+//     age: number;
+//     skills: string[];
+// } = {
+//     name: "John",
+//     age: 25,
+//     skills:["C++","Java","Python"],
+// }
+// console.log(person)
+// tuple
+// const person : {
+//     name: string;
+//     age: number;
+//     skills: string[];
+//     product: [number,string]; //tuple
+// } = {
+//     name: "John",
+//     age: 25,
+//     skills:["C++","Java","Python"],
+//     product: [1,"laptop"]
+// }
+// person.product = [2,"mobile"]
+// person.product[1] = 20
+// Enum
+// enum Role {
+//     Admin,
+//     User,
+// }
+// const person = {
+//     name: "John",
+//     age: 25,
+//     skills:["C++","Java","Python"],
+//     product: [1,"laptop"],
+//     role: Role.User
+// }
+// if(person.role === Role.Admin){
+//     console.log("Admin")
+// }else if(person.role === Role.User){
+//     console.log("User")
+// }
