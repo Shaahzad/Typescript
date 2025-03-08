b1acfeedfbb9cf969724ebb81bb3350c392c489a
Muhammad shahzad <shehzadmemon147@gmail.com>
2025-03-08 23:51:09 +0500

    learn ts

--- Lecture-3/app.js
+++ Lecture-3/app.js
@@ -1,16 +1,29 @@
 // Union
-function combine(num1, num2) {
+// function combine (num1: number | string, num2: number | string) {
+//     let result;
+//     if(typeof num1 === "number" && typeof num2 === "number") {
+//         result = num1 + num2;
+//     }else{
+//         result = num1.toString() + num2.toString();
+//     }
+//     return result
+// }
+// const combineAges = combine(10, 20)
+// const combineNames = combine("shahzad",  " Memon")
+// console.log(combineAges, combineNames)
+// Type Alias || Surtom type
+// Literal Types
+function combine(num1, num2, conversionType) {
     var result;
-    if (typeof num1 === "number" && typeof num2 === "number") {
-        result = num1 + num2;
+    if (typeof num1 === "number" && typeof num2 === "number" || conversionType === "as-number") {
+        result = +num1 + +num2;
     }
     else {
         result = num1.toString() + num2.toString();
     }
     return result;
 }
-var combineAges = combine(10, 20);
-var combineNames = combine("shahzad", " Memon");
-console.log(combineAges, combineNames);
-// Type Alias || Surtom type
-// Literal Types
+var sum1 = combine("10", "20", "as-number");
+var sum2 = combine(10, 50, "as-number");
+var combineName = combine("shahzad", " Memon", "as-string");
+console.log(sum1, sum2, combineName);
--- Lecture-3/app.ts
+++ Lecture-3/app.ts
@@ -13,6 +13,23 @@
 // console.log(combineAges, combineNames)
 
 // Type Alias || Surtom type
+
+
+
+
+
 // Literal Types
 
-
+function combine (num1: number | string, num2: number | string, conversionType: "as-number" | "as-string") {
+    let result;
+    if(typeof num1 === "number" && typeof num2 === "number" || conversionType === "as-number") {
+        result = +num1 + +num2;
+    }else{
+        result = num1.toString() + num2.toString();
+    }
+    return result
+}
+const sum1 = combine("10", "20", "as-number")
+const sum2 = combine(10, 50, "as-number")
+const combineName = combine("shahzad",  " Memon", "as-string")
+console.log(sum1, sum2, combineName)
