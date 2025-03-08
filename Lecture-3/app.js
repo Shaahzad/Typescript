666e3803f78b054c65d859eae2494c73a07e30c0
Muhammad shahzad <shehzadmemon147@gmail.com>
2025-03-08 23:58:06 +0500

    learn ts

--- Lecture-3/app.js
+++ Lecture-3/app.js
@@ -11,8 +11,6 @@
 // const combineAges = combine(10, 20)
 // const combineNames = combine("shahzad",  " Memon")
 // console.log(combineAges, combineNames)
-// Type Alias || Surtom type
-// Literal Types
 function combine(num1, num2, conversionType) {
     var result;
     if (typeof num1 === "number" && typeof num2 === "number" || conversionType === "as-number") {
@@ -23,7 +21,22 @@ function combine(num1, num2, conversionType) {
     }
     return result;
 }
-var sum1 = combine("10", "20", "as-number");
-var sum2 = combine(10, 50, "as-number");
-var combineName = combine("shahzad", " Memon", "as-string");
-console.log(sum1, sum2, combineName);
+var user = {
+    name: "shahzad",
+    age: 20,
+    skills: ["React", "Node", "TypeScript"]
+};
+// Literal Types
+// function combine (num1: number | string, num2: number | string, conversionType: "as-number" | "as-string") {
+//     let result;
+//     if(typeof num1 === "number" && typeof num2 === "number" || conversionType === "as-number") {
+//         result = +num1 + +num2;
+//     }else{
+//         result = num1.toString() + num2.toString();
+//     }
+//     return result
+// }
+// const sum1 = combine("10", "20", "as-number")
+// const sum2 = combine(10, 50, "as-number")
+// const combineName = combine("shahzad",  " Memon", "as-string")
+// console.log(sum1, sum2, combineName)
--- Lecture-3/app.ts
+++ Lecture-3/app.ts
@@ -12,15 +12,12 @@
 // const combineNames = combine("shahzad",  " Memon")
 // console.log(combineAges, combineNames)
 
-// Type Alias || Surtom type
+// Type Alias || custom type
 
+type Combinable = number | string;
+type ConversionType = "as-number" | "as-string";
 
-
-
-
-// Literal Types
-
-function combine (num1: number | string, num2: number | string, conversionType: "as-number" | "as-string") {
+function combine (num1: Combinable, num2: Combinable, conversionType: ConversionType) {
     let result;
     if(typeof num1 === "number" && typeof num2 === "number" || conversionType === "as-number") {
         result = +num1 + +num2;
@@ -29,7 +26,32 @@ function combine (num1: number | string, num2: number | string, conversionType: 
     }
     return result
 }
-const sum1 = combine("10", "20", "as-number")
-const sum2 = combine(10, 50, "as-number")
-const combineName = combine("shahzad",  " Memon", "as-string")
-console.log(sum1, sum2, combineName)
+
+type User = {
+    name: string;
+    age: number;
+    skills: string[]
+}
+
+const user: User = {
+    name: "shahzad",
+    age: 20,
+    skills: ["React", "Node", "TypeScript"]
+}
+
+
+// Literal Types
+
+// function combine (num1: number | string, num2: number | string, conversionType: "as-number" | "as-string") {
+//     let result;
+//     if(typeof num1 === "number" && typeof num2 === "number" || conversionType === "as-number") {
+//         result = +num1 + +num2;
+//     }else{
+//         result = num1.toString() + num2.toString();
+//     }
+//     return result
+// }
+// const sum1 = combine("10", "20", "as-number")
+// const sum2 = combine(10, 50, "as-number")
+// const combineName = combine("shahzad",  " Memon", "as-string")
+// console.log(sum1, sum2, combineName)
