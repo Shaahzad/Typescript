ab13d504f0eefea93e7c8a33cf46080d5575da0c
Muhammad shahzad <shehzadmemon147@gmail.com>
2025-03-10 19:42:47 +0500

    learn ts

--- Lecture-6/classes.js
+++ Lecture-6/classes.js
@@ -1,2 +1,39 @@
 "use strict";
-console.log("Classes hi");
+// class Department{
+// name: string;
+// constructor(n: string){
+// this.name = n;
+// }
+//  describe(this:Department){
+// console.log('Department: '+this.name);
+//  }
+// }
+// const accounting = new Department('Accounting');
+// accounting.describe()
+// const accountingcopy = {
+//     name: "Mochu",
+//     describe: accounting.describe
+// }
+// accountingcopy.describe()
+// private and public access modifiers
+class Department {
+    constructor(n) {
+        this.name = n;
+        this.employees = [];
+    }
+    describe() {
+        console.log('Department: ' + this.name);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+const accounting = new Department('Accounting');
+accounting.describe();
+accounting.addEmployee('Mochu');
+accounting.addEmployee("Mochu 2");
+accounting.printEmployeeInformation();
--- Lecture-6/classes.ts
+++ Lecture-6/classes.ts
@@ -1,1 +1,46 @@
-console.log("Classes hi");
+// class Department{
+// name: string;
+// constructor(n: string){
+// this.name = n;
+// }
+//  describe(this:Department){
+// console.log('Department: '+this.name);
+//  }
+// }
+
+// const accounting = new Department('Accounting');
+// accounting.describe()
+
+// const accountingcopy = {
+//     name: "Mochu",
+//     describe: accounting.describe
+// }
+
+// accountingcopy.describe()
+
+
+// private and public access modifiers
+class Department {
+    name: string;
+    private employees: string[];
+    constructor(n: string) {
+        this.name = n;
+        this.employees = [];
+    }
+    describe(this: Department) {
+        console.log('Department: ' + this.name);
+    }
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+
+const accounting = new Department('Accounting');
+accounting.describe()
+accounting.addEmployee('Mochu');
+accounting.addEmployee("Mochu 2");
+accounting.printEmployeeInformation();
