914ba1e2511a319e432417f2c8c2aac8ec5bed9c
Muhammad shahzad <shehzadmemon147@gmail.com>
2025-03-11 14:26:49 +0500

    learn ts

--- Lecture-6/classes.js
+++ Lecture-6/classes.js
@@ -16,24 +16,161 @@
 // }
 // accountingcopy.describe()
 // private and public access modifiers
-class Department {
-    constructor(n) {
-        this.name = n;
-        this.employees = [];
-    }
-    describe() {
-        console.log('Department: ' + this.name);
-    }
-    addEmployee(employee) {
-        this.employees.push(employee);
-    }
-    printEmployeeInformation() {
-        console.log(this.employees.length);
-        console.log(this.employees);
-    }
-}
-const accounting = new Department('Accounting');
-accounting.describe();
-accounting.addEmployee('Mochu');
-accounting.addEmployee("Mochu 2");
-accounting.printEmployeeInformation();
+// class Department {
+//     name: string;
+//     private employees: string[];
+//     constructor(n: string) {
+//         this.name = n;
+//         this.employees = [];
+//     }
+//     describe(this: Department) {
+//         console.log('Department: ' + this.name);
+//     }
+//     addEmployee(employee: string) {
+//         this.employees.push(employee);
+//     }
+//     printEmployeeInformation() {
+//         console.log(this.employees.length);
+//         console.log(this.employees);
+//     }
+// }
+// const accounting = new Department('Accounting');
+// accounting.describe()
+// accounting.addEmployee('Mochu');
+// accounting.addEmployee("Mochu 2");
+// accounting.printEmployeeInformation();
+// readonly
+// class Department {
+//     name: string;
+//     private employees: string[];
+//     private readonly id: string;
+//     constructor(n: string) {
+//         this.name = n;
+//         this.employees = [];
+//         this.id = "d1"
+//     }
+//     describe(this: Department) {
+//         console.log('Department: ' + this.name);
+//     }
+//     addEmployee(employee: string) {
+//         this.employees.push(employee);
+//     }
+//     printEmployeeInformation() {
+//         console.log(this.employees.length);
+//         console.log(this.employees);
+//     }
+// }
+// const accounting = new Department('Accounting');
+// accounting.describe()
+// accounting.addEmployee('Mochu');
+// accounting.addEmployee("Mochu 2");
+// accounting.printEmployeeInformation();
+// inheritance
+// overwrite properties & protected access modifier
+// setter & getter
+// class Department {
+//     name: string;
+//     protected employees: string[];
+//     constructor(id: string, n: string) {
+//         this.name = n;
+//         this.employees = [];
+//     }
+//     describe(this: Department) {
+//         console.log('Department: ' + this.name);
+//     }
+//     addEmployee(employee: string) {
+//         this.employees.push(employee);
+//     }
+//     printEmployeeInformation() {
+//         console.log(this.employees.length);
+//         console.log(this.employees);
+//     }
+// }
+// class AccountingDepartment extends Department {
+// constructor(id: string, private reports : string[]){
+//     super(id, "Accounting")
+// }
+// addreports(text: string){
+//     this.reports.push(text)
+// }
+// printreports(){
+//     console.log(this.reports)
+// }
+// addEmployee(employee: string): void {
+//     if(employee === "shahzad"){
+//         return 
+//     }
+//     this.employees.push(employee)
+// }
+// get getreports(){
+//     if(this.reports.length > 0){
+//         return this.reports
+//     }
+//     throw new Error("Report Not Found")
+// }
+// set setreports(value: string){
+// if(!value){
+// throw new Error("Plz pass valid value")
+// }
+// this.reports.push(value)
+// }
+// }
+// const accDep = new AccountingDepartment("d1", [])
+// accDep.addreports("Bugs")
+// accDep.printreports()
+// accDep.addEmployee("memon")
+// accDep.printEmployeeInformation()
+// // setter & getter
+// accDep.setreports = "lala"
+// console.log("getter", accDep.getreports) 
+// static
+// class Department {
+//     name: string;
+//     protected employees: string[];
+//     constructor(id: string, n: string) {
+//         this.name = n;
+//         this.employees = [];
+//     }
+//     describe(this: Department) {
+//         console.log('Department: ' + this.name);
+//     }
+//     addEmployee(employee: string) {
+//         this.employees.push(employee);
+//     }
+//     printEmployeeInformation() {
+//         console.log(this.employees.length);
+//         console.log(this.employees);
+//     }
+//     static getSalary(){
+//      return {
+//         salary: 500000
+//      }
+//     }
+// }
+// const salary = Department.getSalary()
+// console.log(salary)
+// // abstract
+// abstract class Department3 {
+//     name: string;
+//    protected employees: string[] = [];
+//    protected readonly id: string;
+//    constructor(id: string, n: string){
+//    this.id = id;
+//    this.name = n
+//    }
+//    abstract describe(this:Department3) : void
+//    abstract displayName() : void
+// }
+// class subclass extends Department3 {
+//     constructor(id: string, private reports : string[]){
+//      super(id, "Accounting")
+//     }
+//     describe(): void {
+//         console.log("department", this.id)
+//     }
+//     displayName(): void {
+//         console.log("displayName", this.name)
+//     }
+// }
+// const subclass1 = new subclass("d1", [])
+// subclass1.describe()
--- Lecture-6/interface.js
+++ Lecture-6/interface.js
@@ -1,2 +1,91 @@
 "use strict";
-console.log("Interface");
+// interface Person {
+//     name: string;
+//     age: number;
+//     greet(text: string) : void
+// }
+// let user : Person
+// user = {
+//     name: "khan",
+//     age: 20,
+//     greet(text) : void {
+//         console.log(`${text} ${this.name}`)
+//     },
+// }
+// console.log(user)
+// user.greet("hi there shahzad")
+// interface greetable {
+//     name: string;
+//     greet(text: string): void
+// }
+// interface Person {
+//     name: string;
+//     age: number;
+// }
+// class Personc implements greetable  {
+//     name: string;
+//     constructor(n : string){
+//         this.name = n
+//     }
+//     greet(text: string): void {
+//         console.log(`${text} ${this.name}`)
+//     }
+// }
+// const p = new Personc("Aman")
+// p.greet("Hi there ")
+// readonly
+// interface greetable {
+//     readonly name: string;
+//     greet(text: string): void
+// }
+// class Person implements greetable  {
+//     name: string;
+//     constructor(n : string){
+//         this.name = n
+//     }
+//     greet(text: string): void {
+//         console.log(`${text} ${this.name}`)
+//     }
+// }
+// let user1 : greetable ;
+// user1 = new Person("Aman")
+// user1.name = "Shahzad" error
+// Extending interfaces
+// interface Named {
+//     readonly name: string;
+// }
+// interface Greetable extends Named {
+//     greet(text: string): void
+// }
+// class Person1 implements Greetable  {
+//     name: string;
+//     constructor(n : string){
+//         this.name = n
+//     }
+//     greet(text: string): void {
+//         console.log(`${text} ${this.name}`)
+//     }
+// }
+// let user1 : Greetable ;
+// user1 = new Person1("Aman")
+// user1.greet("Hi there")
+// type AddFn = (a: number, b: number) => number;
+// interface AddFun {
+//     (a: number, b: number): number;
+// }
+// let add: AddFun;
+// add = (n1: number, n2: number) => {
+//     return n1 + n2;
+// }
+// console.log(add(5, 5))
+// optional
+// interface Named {
+//     readonly name: string;
+//     outputName?: string;
+// }
+// class Person implements Named  {
+//     name: string;
+//     constructor(n : string){
+//         this.name = n
+//     }
+// }
